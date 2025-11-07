5624825f1188c7e68f44df5e0905b2ec86b737dc
dimitrije <dimitrije.jelenkovic@digitalcube.rs>
2025-11-07 17:00:07 +0100

    added ng zorro

--- CarbonLess/src/app/app.config.ts
+++ CarbonLess/src/app/app.config.ts
@@ -1,5 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideNzI18n, en_US } from 'ng-zorro-antd/i18n';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
@@ -7,6 +10,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    provideNzI18n(en_US)
   ]
 };
--- CarbonLess/src/app/prompt-dashboard/prompt-dashboard.component.ts
+++ CarbonLess/src/app/prompt-dashboard/prompt-dashboard.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 @Component({
   selector: 'app-prompt-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NzInputModule],
   templateUrl: './prompt-dashboard.component.html',
   styleUrls: ['./prompt-dashboard.component.scss']
 })
 export class PromptDashboardComponent {
-
+  value: any = '';
 }
