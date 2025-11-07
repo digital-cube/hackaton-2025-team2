8ce3d4718ef6f6e91e4860746ac1c02d28c47183
marko <marko.janovic@digitalcube.rs>
2025-11-07 22:02:13 +0100

    Models, env handler
    - added models, aerich and auto config generator
    - added env handler

--- CarbonLess_backend/src/main.py
+++ CarbonLess_backend/src/main.py
@@ -3,15 +3,18 @@ from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 
+from src.utils.config import init_db, get_tortoise_orm_config
 from src.utils.fastapi_utils import load_services
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     load_services(app)
-    # await init_db(app)
+    await init_db(app)
     yield
 app = FastAPI(lifespan=lifespan)
+
+TORTOISE_ORM = get_tortoise_orm_config()
 
 if __name__ == "__main__":
     uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
