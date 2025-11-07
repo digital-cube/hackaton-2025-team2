deb7e9da9467b98f9c817a482fb5f15e47da0cf9
dimitrije <dimitrije.jelenkovic@digitalcube.rs>
2025-11-07 17:21:47 +0100

    main form submission done

--- CarbonLess/src/app/main-form/main-form.component.ts
+++ CarbonLess/src/app/main-form/main-form.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-main-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './main-form.component.html',
   styleUrls: ['./main-form.component.scss']
 })
 export class MainFormComponent {
+  mainForm = new FormGroup({
+    location: new FormControl(''),
+    transport: new FormControl(''),
+    days: new FormControl('')
+  })
 
+  onSubmit() {
+    console.log('forma submit', this.mainForm)
+  }
 }
